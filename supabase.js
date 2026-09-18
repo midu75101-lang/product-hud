d23fd1b347ef9c90004ffec32684dcc7c96121ed
midu75101-lang <midu75101@gmail.com>
2026-09-18 12:10:16 -0700

    Create supabase.js

---
+++ supabase.js
@@ -0,0 +1,7 @@
+const SUPABASE_URL = "PASTE_PROJECT_URL_HERE";
+const SUPABASE_PUBLISHABLE_KEY = "PASTE_PUBLISHABLE_KEY_HERE";
+
+const supabaseClient = window.supabase.createClient(
+    mbjyejuvlcwajbupstje
+    sb_publishable_jF_DisY3Cw2IQAmioDUr_Q_eWZdMavk
+);
